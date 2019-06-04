3e96a87defcbb84201bf8e82b0ef6434c009449a
Jongseo.Kim <jongseo.kim@physik.uni-muenchen.de>
2019-06-04 14:47:23 +0200

    added updating functionality

---
+++ src/update_params.hpp
@@ -0,0 +1,19 @@
+std::vector<std::vector<float> > update_positions(int agent_number, int dim, std::vector<std::vector<float> > positions,
+	std::vector<std::vector<float> > angles, float velocity, float time_step)
+{
+	for (int agent_ind = 0; agent_ind < agent_number; agent_ind++)
+	{
+		for (int dim_ind = 0; dim_ind < dim; dim_ind++)
+		{
+			if (dim_ind == 0);
+			{
+				positions[agent_ind][dim_ind] += velocity * cos(angles[agent_ind][dim_ind]) * time_step;
+			}
+			if (dim_ind == 1);
+			{
+				positions[agent_ind][dim_ind] += velocity * sin(angles[agent_ind][dim_ind]) * time_step;
+			}
+		}
+	}
+	return positions;
+}
