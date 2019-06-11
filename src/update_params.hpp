ddf97170c855a30a8f29683c4063652d3d04f64a
Jongseo.Kim <jongseo.kim@physik.uni-muenchen.de>
2019-06-11 17:09:12 +0200

    add update_angles but not finished yet

--- src/update_params.hpp
+++ src/update_params.hpp
@@ -7,8 +7,10 @@ void update_positions(int agent_number, int dim, std::vector<std::vector<float> 
 		{
 			if (dim_ind == 0)
 			{
+				//differential equation
 				positions[agent_ind][dim_ind] += velocity * cos(angles[agent_ind][dim_ind]) * time_step;
 
+				//periodic boundary condition
 				if (positions[agent_ind][dim_ind] > box_size)
 				{
 					positions[agent_ind][dim_ind] -= box_size;
@@ -20,7 +22,10 @@ void update_positions(int agent_number, int dim, std::vector<std::vector<float> 
 			}
 			if (dim_ind == 1)
 			{
+				//differential equation
 				positions[agent_ind][dim_ind] += velocity * sin(angles[agent_ind][dim_ind]) * time_step;
+				
+				//periodic boundary condition
 				if (positions[agent_ind][dim_ind] > box_size)
 				{
 					positions[agent_ind][dim_ind] -= box_size;
@@ -34,3 +39,23 @@ void update_positions(int agent_number, int dim, std::vector<std::vector<float> 
 		}
 	}
 }
+
+void update_angles(int agent_number, int dim, std::vector<std::vector<float> > angles,
+	float time_step, float noise_strength, std::vector<std::vector<int> > interacting_neighbors )
+{
+	// initialize random number generator, draw from [-pi;pi)
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> dis(-atan(1) * 4, atan(1) * 4);
+
+	for (int agent_ind = 0; agent_ind < agent_number; agent_ind++)
+	{
+		for (int dim_ind = 0; dim_ind < dim - 1; dim_ind++)
+		{
+			if (dim_ind == 0)
+			{
+				//stochastic differential equation
+				angles[agent_ind][dim_ind] = mean(angles) + noise_strength * dis(gen);
+			}
+		}
+	}
